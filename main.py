c21aeca7eba131ab7cc7351e90bae7e04697f668
dantekruls1 <dfolamide15@gmail.com>
2023-09-20 13:33:15 +0100

    hello

---
+++ main.py
@@ -0,0 +1,76 @@
+# import pandas as pd
+
+# mydataset = {
+#   'cars': ["BMW", "Volvo", "Ford"],
+#   'passings': [3, 7, 2]
+# }
+
+# myvar = pd.DataFrame(mydataset)
+
+# print(myvar)
+# import pandas as pd
+
+# print(pd.__version__)
+# import pandas as pd
+
+# a = [1, 7, 2]
+
+# myvar = pd.Series(a)
+# print(a[0])
+# print(myvar[0])
+
+# print(myvar)
+
+# import pandas as pd
+
+# data = {
+#   "calories": [420, 380, 390],
+#   "duration": [50, 40, 45]
+# }
+
+# myvar = pd.DataFrame(data)
+
+# print(myvar)
+
+# import pandas as pd
+
+# df = pd.read_csv('data.csv')
+# print(df)
+# print(df.to_string())
+# # print(df.to_string()) 
+
+
+
+# print(pd.options.display.max_rows) 
+
+# import pandas as pd
+
+# df = pd.read_json('data.json')
+
+# print(df.to_string())
+
+
+
+# import pandas as pd 
+# df = pd.read_csv('income.csv')
+
+
+# df['net_income'] = df['revenue'] - df['operational cost']
+
+# df['revenue2'] = df['net_income'] + df['operational cost'] 
+# df.to_csv('income.csv', index=False)
+# print(df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
